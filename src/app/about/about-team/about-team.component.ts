fb4ddf09015b3c5d23045de412cbc0ef8300bdf4
Meryhanwahed <meroowahed3@gmail.com>
2025-04-08 06:09:50 +0200

    stylee retouches

--- src/app/about/about-team/about-team.component.ts
+++ src/app/about/about-team/about-team.component.ts
@@ -17,20 +17,20 @@ export class TeamComponent {
         'Merihan Wahed is the CEO of Realview Frontend Group, leading innovative frontend solutions with expertise in Angular and UI/UX design.',
     },
     {
-      name: 'Zeeshan Ali Khan',
-      position: 'CEO, Dubizzle Group Pakistan',
+      name: 'Merihan Wahed',
+      position: 'CEO, Realview Frontend Group',
       role: 'Board Member',
-      image: 'assets/zeeshan.jpg',
+      image: 'assets/images/aboutPage/team/merihan.jpg',
       description:
-        'Zeeshan Ali Khan is a member of the Dubizzle Group Board of Directors and the company’s co-founder. The quintessential entrepreneur.',
+        'Merihan Wahed is the CEO of Realview Frontend Group, leading innovative frontend solutions with expertise in Angular and UI/UX design.',
     },
     {
       name: 'Haider Ali Khan',
-      position: 'CEO, Dubizzle Group MENA',
-      role: 'Board Member',
-      image: 'assets/haider.jpg',
-      description:
-        'Haider Ali Khan is the tech visionary leading Bayut-Dubizzle into its promising future. Educated at the University of Texas at Austin.',
-    },
+        position: 'CEO, Realview Frontend Group',
+        role: 'Board Member',
+        image: 'assets/images/aboutPage/team/merihan.jpg',
+        description:
+          'Merihan Wahed is the CEO of Realview Frontend Group, leading innovative frontend solutions with expertise in Angular and UI/UX design.',
+      },
   ];
 }
--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,10 +3,11 @@ import { AboutSummaryComponent } from './about-summary/about-summary.component';
 import { AboutContactComponent } from './about-contact/about-contact.component';
 import { TeamComponent } from "./about-team/about-team.component";
 import { AboutBannerComponent } from "./about-banner/about-banner.component";
+import { CarouselComponent } from "../components/carousel/carousel.component";
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [AboutSummaryComponent, AboutContactComponent, TeamComponent, AboutBannerComponent],
+  imports: [AboutSummaryComponent, AboutContactComponent, TeamComponent, CarouselComponent],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
